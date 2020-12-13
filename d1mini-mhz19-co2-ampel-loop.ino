a4634197005845a28942519dbe0daa89ff56c875
Petra Tenzlinger <petra@tenzlinger.de>
2020-12-13 13:10:35 +0100

    no violet but red blinking; millis instead of delay

--- d1mini-mhz19-co2-ampel-loop.ino
+++ d1mini-mhz19-co2-ampel-loop.ino
@@ -8,11 +8,13 @@
  Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 int CO2Wert;  // CO2 Wert in ppm
-int Messinterval = 1; // alle x Minuten messen
 
 // TX, RX Pins festlegen
 SoftwareSerial co2Serial(D7, D6);
 
+unsigned long startMillis;
+unsigned long currentMillis;
+const unsigned long period = 1000*20;  // milliseconds
 
 void setup() {
   Serial.begin(115200);               // serielle Kommunikation mit dem PC beginnen
@@ -31,40 +33,56 @@ void setup() {
   // einmal blinken bitte
   delay(10);
   blinkInAllColors();
+  pixels.setPixelColor(0, pixels.Color(0, 0, 255)); //blue
+  pixels.show();
+  
+  startMillis = millis();  // Startzeit
 }
  
 void loop()
 {
   // am besten abschalten im Klassenzimmer ;-)
   //blinkInAllColors();
-  
-  // MH-Z19 CO2 Sensor lesen
-  CO2Wert = leseCO2();      
+   
+  currentMillis = millis();  // ms seit Programmstart
+  if (currentMillis - startMillis >= period)  // 
+  {
+    // neu messen
+    // MH-Z19 CO2 Sensor lesen
+    CO2Wert = leseCO2();      
             
-  // Ampelfarbe einstellen
-  if (CO2Wert < 0){
-    pixels.setPixelColor(0, pixels.Color(0, 0, 255)); // blau=Fehler (oder Neustart)
-    
-  } else if (CO2Wert < 1000) {
-    pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // grün 
-    
-  } else if (CO2Wert < 1400) {
-    pixels.setPixelColor(0, pixels.Color(255, 153, 0)); // gelb
-    
-  } else {
-    pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // rot
-  }
-  if (CO2Wert > 2000) {
-    pixels.setPixelColor(0, pixels.Color(153, 0, 255)); // violett
-  }
-  // eingestellte Pixelfarbe an die Lampe senden 
-  pixels.show(); 
+    // Ampelfarbe einstellen
+    if (CO2Wert < 0){
+      pixels.setPixelColor(0, pixels.Color(0, 0, 255)); // blau=Fehler (oder Neustart)    
+    } else if (CO2Wert < 1000) {
+      pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // grün 
+      
+    } else if (CO2Wert < 1400) {
+      pixels.setPixelColor(0, pixels.Color(255, 153, 0)); // gelb
+      
+    } else {
+      pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // rot
+    }
+    // eingestellte Pixelfarbe an die Lampe senden 
+    pixels.show(); 
 
-  // Ausgabe
-  //Serial.printf("CO2 = %d ppm\n\n", CO2Wert);
-  Serial.print("CO2[ppm]:"); Serial.println(CO2Wert);
-  //Serial.print("Pause für "); Serial.print(Messinterval); Serial.println(" min");
-  delay(Messinterval * 1000 * 60);  // in Millisekunden umrechnen
+    // Ausgabe
+    //Serial.printf("CO2 = %d ppm\n\n", CO2Wert);
+    Serial.print("CO2[ppm]:"); Serial.println(CO2Wert);
+    //Serial.print("Pause für "); Serial.print(Messinterval); Serial.println(" min");
+    startMillis = currentMillis;  // neue Startzeit
+  } else {
+    // wenn CO2 höher als 2000, dann rot blinken
+    if (CO2Wert > 2000) {
+      //Serial.println("CO2 über 2000!");
+      delay(200);
+      pixels.setPixelColor(0,0,0,0);  // Licht aus
+      pixels.show();
+      delay(200);
+      pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // rot
+      pixels.show(); 
+    }
+  }
 }
 
 // misst CO2 Wert und liefert ihn zurück in ppm 
